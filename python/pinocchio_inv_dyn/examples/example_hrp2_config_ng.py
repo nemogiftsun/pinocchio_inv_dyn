482333b7c5018df1b08c1612c5464af5a6930e68
ngiftsun <nirmalgibbs@gmail.com>
2017-02-13 17:59:45 +0100

    Added config with capture point inequalities added

---
+++ python/pinocchio_inv_dyn/examples/example_hrp2_config_ng.py
@@ -0,0 +1,96 @@
+from math import sqrt
+import numpy as np
+
+''' *********************** USER-PARAMETERS *********************** '''
+SOLVER_ID       = 0;    # classic TSID formulation
+SOLVER_TO_INTEGRATE         = [SOLVER_ID];
+DATA_FILE_NAME              = 'data';
+TEXT_FILE_NAME              = 'results.txt';
+SAVE_DATA                   = True;
+
+''' INITIAL STATE PARAMETERS '''
+MAX_TEST_DURATION           = 3000;
+dt                          = 1e-3;
+model_path                  = ["/opt/openrobots/share"];
+urdfFileName                = model_path[0] + "/hrp2_14_description/urdf/hrp2_14_reduced.urdf";
+freeFlyer                   = True;
+q0 = np.matrix([0.0, 0.0, 0.648702, 0.0, 0.0 , 0.0, 1.0,                             # Free flyer 0-6
+                0.0, 0.0, 0.0, 0.0,                                                  # CHEST HEAD 7-10
+                0.261799388,  0.174532925, 0.0, -0.523598776, 0.0, 0.0, 0.174532925, # LARM       11-17
+                0.261799388, -0.174532925, 0.0, -0.523598776, 0.0, 0.0, 0.174532925, # RARM       18-24
+                0.0, 0.0, -0.453785606, 0.872664626, -0.41887902, 0.0,               # LLEG       25-30
+                0.0, 0.0, -0.453785606, 0.872664626, -0.41887902, 0.0,               # RLEG       31-36
+                ]).T;
+v0 = np.matrix(np.zeros(36)).T;
+
+''' CONTROLLER CONFIGURATION '''
+ENABLE_CAPTURE_POINT_LIMITS     = True;
+ENABLE_TORQUE_LIMITS            = True;
+ENABLE_FORCE_LIMITS             = True;
+ENABLE_JOINT_LIMITS             = True;
+IMPOSE_POSITION_BOUNDS          = True;
+IMPOSE_VELOCITY_BOUNDS          = True;
+IMPOSE_VIABILITY_BOUNDS         = True;
+IMPOSE_ACCELERATION_BOUNDS      = True;
+JOINT_POS_PREVIEW               = 1.5; # preview window to convert joint pos limits into joint acc limits
+JOINT_VEL_PREVIEW               = 1;   # preview window to convert joint vel limits into joint acc limits
+MAX_JOINT_ACC                   = 30.0;
+MAX_MIN_JOINT_ACC               = 10.0;
+USE_JOINT_VELOCITY_ESTIMATOR    = False;
+ACCOUNT_FOR_ROTOR_INERTIAS      = True;
+
+# CONTROLLER GAINS
+kp_posture  = 30; #1.0;   # proportional gain of postural task
+kd_posture  = 2*sqrt(kp_posture);
+kp_constr   = 100.0;   # constraint proportional feedback gain
+kd_constr   = 2*sqrt(kp_constr);   # constraint derivative feedback gain
+kp_com      = 10.0;
+kd_com      = 2*sqrt(kp_com);
+kp_ee       = 100.0;
+kd_ee       = 2*sqrt(kp_ee);
+constraint_mask = np.array([True, True, True, True, True, True]).T;
+ee_mask         = np.array([True, True, True, True, True, True]).T;
+
+# CONTROLLER WEIGTHS
+w_com           = 1;
+w_posture       = 1e-3;  # weight of postural task
+
+# QP SOLVER PARAMETERS
+maxIter = 300;      # max number of iterations
+maxTime = 0.8;      # max computation time for the solver in seconds
+verb=0;             # verbosity level (0, 1, or 2)
+
+# CONTACT PARAMETERS
+mu  = np.array([0.3, 0.1]);          # force and moment friction coefficient
+fMin = 1e-3;					     # minimum normal force
+
+''' SIMULATOR PARAMETERS '''
+FORCE_TORQUE_LIMITS            = ENABLE_TORQUE_LIMITS;
+FORCE_JOINT_LIMITS             = ENABLE_JOINT_LIMITS and IMPOSE_POSITION_BOUNDS;
+USE_LCP_SOLVER                 = False
+
+''' STOPPING CRITERIA THRESHOLDS '''
+MAX_CONSTRAINT_ERROR        = 0.1;
+
+'''INERTIAL ERROR'''
+MAX_COM_ERROR = 0.01
+MAX_MASS_ERROR = 0.1
+MAX_INERTIA_ERROR = 0.01
+
+''' INITIAL STATE PARAMETERS '''
+INITIAL_CONFIG_ID                   = 0;
+INITIAL_CONFIG_FILENAME             = '../../../data/hrp2_configs_coplanar';
+
+''' VIEWER PARAMETERS '''
+ENABLE_VIEWER               = True;
+PLAY_MOTION_WHILE_COMPUTING = True;
+PLAY_MOTION_AT_THE_END      = True;
+DT_VIEWER                   = 10*dt;   # timestep used to display motion with viewer
+SHOW_VIEWER_FLOOR           = True;
+
+''' FIGURE PARAMETERS '''
+SAVE_FIGURES     = False;
+SHOW_FIGURES     = False;
+SHOW_LEGENDS     = True;
+LINE_ALPHA       = 0.7;
+#BUTTON_PRESS_TIMEOUT        = 100.0;
